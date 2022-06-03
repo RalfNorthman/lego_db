058eeb2c9b0d137b14f2ec0fff41bf55a0d6fe7e
Ralf Northman <ralf.northman@bredband.net>
2022-06-03 18:31:43 +0200

    Set foreign keys, change column names etc.

--- create_tables.sql
+++ create_tables.sql
@@ -1,13 +1,15 @@
+drop database lego;
+create database lego;
+
 use lego;
 
-
 create or replace table part_categories(
-  part_category_id tinyint unsigned not null primary key,
-  part_category_name char(50) not null
+  id tinyint unsigned not null primary key,
+  name char(50) not null
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/part_categories.csv' 
+  'csvs\\part_categories.csv' 
   into table part_categories
   columns
     terminated by ','
@@ -17,31 +19,14 @@ load data local infile
 select * from part_categories limit 10;
 
 
-create or replace table themes(
-  theme_id smallint unsigned not null primary key,
-  theme_name char(50) not null,
-  parent_id smallint unsigned not null
-);
-
-load data local infile 
-  '/home/ralf/Programming/sql/lego/themes.csv' 
-  into table themes
-  columns
-    terminated by ','
-    optionally enclosed by '"'
-  ignore 1 lines;
-
-select * from themes limit 10;
-
-
 create or replace table parts(
-  part_id char(16) not null primary key,
-  part_name tinytext not null,
-  part_category_id tinyint unsigned not null
+  id char(16) not null primary key,
+  name tinytext not null,
+  category_id tinyint unsigned not null references part_categories(id)
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/parts.csv' 
+  'csvs\\parts.csv' 
   into table parts
   columns
     terminated by ','
@@ -51,16 +36,33 @@ load data local infile
 select * from parts limit 10;
 
 
+create or replace table themes(
+  id smallint unsigned not null primary key,
+  name char(50) not null,
+  parent_id smallint unsigned not null
+);
+
+load data local infile 
+  'csvs\\themes.csv' 
+  into table themes
+  columns
+    terminated by ','
+    optionally enclosed by '"'
+  ignore 1 lines;
+
+select * from themes limit 10;
+
+
 create or replace table sets(
-  set_id char(16) not null primary key,
-  set_name tinytext not null,
+  char(16) not null primary key,
+  name tinytext not null,
   year smallint unsigned not null,
-  theme_id smallint unsigned not null,
+  theme_id smallint unsigned not null references themes(id),
   num_parts smallint unsigned not null
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/sets.csv' 
+  'csvs\\sets.csv' 
   into table sets
   columns
     terminated by ','
@@ -71,13 +73,13 @@ select * from sets limit 10;
 
 
 create or replace table inventories(
-  inventory_id smallint unsigned not null primary key,
-  version tinyint not null,
-  set_id char(16) not null
+  id smallint unsigned not null primary key,
+  version_nr tinyint not null,
+  set_id char(16) not null references sets(id)
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/inventories.csv' 
+  'csvs\\inventories.csv' 
   into table inventories
   columns
     terminated by ','
@@ -88,14 +90,14 @@ select * from inventories limit 10;
 
 
 create or replace table inventory_sets(
-  inventory_id smallint unsigned not null,
-  set_id char(16) not null,
+  inventory_id smallint unsigned nnull references inventories(id),
+  set_id char(16) not null references sets(id),
   quantity tinyint unsigned not null,
   primary key (inventory_id, set_id)
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/inventory_sets.csv' 
+  'csvs\\inventory_sets.csv' 
   into table inventory_sets
   columns
     terminated by ','
@@ -105,34 +107,15 @@ load data local infile
 select * from inventory_sets limit 10;
 
 
-create or replace table inventory_parts(
-  inventory_id smallint unsigned not null,
-  part_id char(16) not null,
-  color_id smallint unsigned not null,
-  quantity smallint unsigned not null,
-  is_spare enum('t', 'f') not null
-);
-
-load data local infile 
-  '/home/ralf/Programming/sql/lego/inventory_parts.csv' 
-  into table inventory_parts
-  columns
-    terminated by ','
-    optionally enclosed by '"'
-  ignore 1 lines;
-
-select * from inventory_parts limit 10;
-
-
 create or replace table colors(
-  color_id mediumint not null primary key,
-  color_name char(32) not null,
+  id smallint not null primary key,
+  name char(32) not null,
   rgb char(6) not null,
   is_trans enum('t', 'f') not null
 );
 
 load data local infile 
-  '/home/ralf/Programming/sql/lego/colors.csv' 
+  'csvs\\colors.csv' 
   into table colors
   columns
     terminated by ','
@@ -140,3 +123,23 @@ load data local infile
   ignore 1 lines;
 
 select * from colors limit 10;
+
+
+create or replace table inventory_parts(
+  inventory_id smallint unsigned not null references inventories(id),
+  part_id char(16) not null references parts(id),
+  color_id smallint not null references colors(id),
+  quantity smallint unsigned not null,
+  is_spare enum('t', 'f') not null,
+  primary key (inventory_id, part_id, color_id)
+);
+
+load data local infile 
+  'csvs\\inventory_parts.csv' 
+  into table inventory_parts
+  columns
+    terminated by ','
+    optionally enclosed by '"'
+  ignore 1 lines;
+
+select * from inventory_parts limit 10;
